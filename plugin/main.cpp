36a889ef81673908688203ac68d99e490d6972f3
Phillip Lorentz <plorentz7@gmail.com>
2025-04-05 12:02:33 -0500

    Update main.cpp

--- plugin/main.cpp
+++ plugin/main.cpp
@@ -1,25 +1,90 @@
 #include <reshade.hpp>
 #include <reshade_api.hpp>
-#include <algorithm>
+#include <unordered_map>
+
+// Windows Header Files:
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <tchar.h>
+#include <Psapi.h>
+#include <utility>
+#include <vector>
 
 extern "C" __declspec(dllexport) const char* NAME = "ReShade Zoom Plugin";
 extern "C" __declspec(dllexport) const char* DESCRIPTION = "A plugin for handling zooming in of the screen to simulate variable zoom scopes.";
 
+static std::unordered_map<reshade::api::effect_runtime *, bool> toggle_states;
+
+
+// from ReShade
+static const char *keyboard_keys[256] = {
+	"", "Left Mouse", "Right Mouse", "Cancel", "Middle Mouse", "X1 Mouse", "X2 Mouse", "", "Backspace", "Tab", "", "", "Clear", "Enter", "", "",
+	"Shift", "Control", "Alt", "Pause", "Caps Lock", "", "", "", "", "", "", "Escape", "", "", "", "",
+	"Space", "Page Up", "Page Down", "End", "Home", "Left Arrow", "Up Arrow", "Right Arrow", "Down Arrow", "Select", "", "", "Print Screen", "Insert", "Delete", "Help",
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "", "", "", "", "", "",
+	"", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+	"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Left Windows", "Right Windows", "Apps", "", "Sleep",
+	"Numpad 0", "Numpad 1", "Numpad 2", "Numpad 3", "Numpad 4", "Numpad 5", "Numpad 6", "Numpad 7", "Numpad 8", "Numpad 9", "Numpad *", "Numpad +", "", "Numpad -", "Numpad Decimal", "Numpad /",
+	"F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "F13", "F14", "F15", "F16",
+	"F17", "F18", "F19", "F20", "F21", "F22", "F23", "F24", "", "", "", "", "", "", "", "",
+	"Num Lock", "Scroll Lock", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+	"Left Shift", "Right Shift", "Left Control", "Right Control", "Left Menu", "Right Menu", "Browser Back", "Browser Forward", "Browser Refresh", "Browser Stop", "Browser Search", "Browser Favorites", "Browser Home", "Volume Mute", "Volume Down", "Volume Up",
+	"Next Track", "Previous Track", "Media Stop", "Media Play/Pause", "Mail", "Media Select", "Launch App 1", "Launch App 2", "", "", "OEM ;", "OEM +", "OEM ,", "OEM -", "OEM .", "OEM /",
+	"OEM ~", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
+	"", "", "", "", "", "", "", "", "", "", "", "OEM [", "OEM \\", "OEM ]", "OEM '", "OEM 8",
+	"", "", "OEM <", "", "", "", "", "", "", "", "", "", "", "", "", "",
+	"", "", "", "", "", "", "Attn", "CrSel", "ExSel", "Erase EOF", "Play", "Zoom", "", "PA1", "OEM Clear", ""
+};
+
+std::string codeToString(uint8_t vkCode){
+	return keyboard_keys[vkCode];
+}
+
+uint8_t stringToCode(std::string key){
+	for (int i=0; i<256; i++) {
+		if (keyboard_keys[i] == key) return i;
+	}
+	return 0;
+}
+
+bool isKeyDown(const reshade::api::effect_runtime* runtime, std::string _keyStr, bool _altRequired, bool _shiftRequired, bool _ctrlRequired){
+	uint8_t _keyCode = stringToCode(_keyStr);
+	bool toReturn = false;
+	toReturn = runtime->is_key_down(_keyCode);
+	const bool altPressed = runtime->is_key_down(VK_MENU);;
+	const bool shiftPressed = runtime->is_key_down(VK_SHIFT);
+	const bool ctrlPressed = runtime->is_key_down(VK_CONTROL);
+
+	toReturn &= ((_altRequired && altPressed) || (!_altRequired && !altPressed));
+	toReturn &= ((_shiftRequired && shiftPressed) || (!_shiftRequired && !shiftPressed));
+	toReturn &= ((_ctrlRequired && ctrlPressed) || (!_ctrlRequired && !ctrlPressed));
+	return toReturn;
+}
+
+bool isKeyPressed(const reshade::api::effect_runtime* runtime, std::string _keyStr, bool _altRequired, bool _shiftRequired, bool _ctrlRequired){
+	uint8_t _keyCode = stringToCode(_keyStr);
+	bool toReturn = false;
+	toReturn = runtime->is_key_pressed(_keyCode);
+	const bool altPressed = runtime->is_key_down(VK_MENU);;
+	const bool shiftPressed = runtime->is_key_down(VK_SHIFT);
+	const bool ctrlPressed = runtime->is_key_down(VK_CONTROL);
+
+	toReturn &= ((_altRequired && altPressed) || (!_altRequired && !altPressed));
+	toReturn &= ((_shiftRequired && shiftPressed) || (!_shiftRequired && !shiftPressed));
+	toReturn &= ((_ctrlRequired && ctrlPressed) || (!_ctrlRequired && !ctrlPressed));
+	return toReturn;
+}
+
 // Callback to listen for keyboard input
 static void onReshadePresent(reshade::api::effect_runtime* runtime) {
-    const reshade::api::effect_uniform_variable zoom_var = runtime->find_uniform_variable("zoomscope.fx", "DynamicZoomLevel");
-    const reshade::api::effect_uniform_variable wheel_var = runtime->find_uniform_variable("zoomscope.fx", "MouseWheelDelta");
-    const reshade::api::effect_uniform_variable scale_var = runtime->find_uniform_variable("zoomscope.fx", "ZoomLevelDelta");
-    const reshade::api::effect_uniform_variable enable_var = runtime->find_uniform_variable("zoomscope.fx", "EnableMagnifier");
-    if (zoom_var == 0 || wheel_var == 0 || scale_var == 0 || enable_var == 0) return;
-    bool enabled = false;
-    runtime->get_uniform_value_bool(enable_var, &enabled, 1);
-    if (!enabled) {
-        float DynamicZoomLevel = 1.0f;
-        runtime->set_uniform_value_float(zoom_var, &DynamicZoomLevel, 1);
-    } else {
-        float DynamicZoomLevel = 2.0f;
-        runtime->set_uniform_value_float(zoom_var, &DynamicZoomLevel, 1);
+    if (!isKeyPressed(runtime, "Right Mouse", false, false, false)) {
+        // Set uniform variable in shader
+	const reshade::api::effect_uniform_variable synced_variable = runtime->find_uniform_variable("zoomscope.fx", "MouseWheelDelta");
+        if (synced_variable != 0) {
+            float data[2] = { 0.0f, 0.0f };
+            runtime->set_uniform_value_float(synced_variable, &data[0], 2);
+        }
     }
 }
 
