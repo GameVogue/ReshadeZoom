b672ceb2961e7a0acdddb1039b6d7460bb652b44
Phillip Lorentz <plorentz7@gmail.com>
2025-04-04 22:08:09 -0500

    Update main.cpp

--- plugin/main.cpp
+++ plugin/main.cpp
@@ -1,22 +1,22 @@
 #include <reshade.hpp>
 #include <unordered_map>
+#include "KeyData.h"
 
 extern "C" __declspec(dllexport) const char* NAME = "ReShade Zoom Plugin";
 extern "C" __declspec(dllexport) const char* DESCRIPTION = "A plugin for handling zooming in of the screen to simulate variable zoom scopes.";
 
 static std::unordered_map<reshade::api::effect_runtime *, bool> toggle_states;
+static KeyData _keyData;
 
 // Callback to listen for keyboard input
-void on_input(reshade::api::effect_runtime *runtime, const reshade::input::input_event &event) {
-    if (event.type == reshade::input::input_event_type::key_down &&
-        event.key_data.vkey == VK_F2) // Use F2 as the toggle key
-    {
+static void onReshadePresent(effect_runtime* runtime)
+    if (_keyData.isKeyPressed(runtime)) {
         // Variable state
         bool &state = toggle_states[runtime];
         state = !state;
 
         // Set uniform variable in shader
-		const effect_uniform_variable synced_variable = runtime->find_uniform_variable({}, "DynamicZoomLevel");
+	const effect_uniform_variable synced_variable = runtime->find_uniform_variable({}, "DynamicZoomLevel");
         if (synced_variable != nullptr) {
             float value = state ? 2.0f : 1.0f;
             runtime->set_uniform_value_float(synced_variable, static_cast<const float *>(value), 1);
@@ -27,11 +27,11 @@ void on_input(reshade::api::effect_runtime *runtime, const reshade::input::input
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID) {
     if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         reshade::register_addon(hModule);
-        reshade::register_event<reshade::addon_event::input_event>(on_input);
+	reshade::register_event<reshade::addon_event::reshade_present>(onReshadePresent);
+    	_keyData.setKey("F2", false, false, false);
     } else if (ul_reason_for_call == DLL_PROCESS_DETACH) {
-        reshade::unregister_event<reshade::addon_event::input_event>(on_input);
+	reshade::unregister_event<reshade::addon_event::reshade_present>(onReshadePresent);
         reshade::unregister_addon(hModule);
     }
     return TRUE;
 }
-
